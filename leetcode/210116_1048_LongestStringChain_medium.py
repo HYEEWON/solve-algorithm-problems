8df158ea371f22e9b5c197a065977bccf72382e2
HYEEWON <hw0916@naver.com>
2021-01-16 23:06:01 +0900

    1048 dp

---
+++ leetcode/210116_1048_LongestStringChain_medium.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/longest-string-chain/
+import math
+import os
+import random
+import re
+import sys
+
+from collections import defaultdict
+d = defaultdict(int)
+        
+def longestChain(words):
+    words = list(set(words))
+    words = sorted(words, key = lambda x: (len(x), x))
+    for i in range(0, len(words)):
+        word = words[i]
+        d[word] = 1
+        for j in range(0, len(word)):
+            tmp = word[:j]+word[j+1:]
+            #print(word, tmp)
+            if tmp in d.keys():
+                d[word] = max(d[word], d[tmp]+1)
+            #print(d)
+    return max(list(d.values()))
+
+if __name__ == '__main__':
