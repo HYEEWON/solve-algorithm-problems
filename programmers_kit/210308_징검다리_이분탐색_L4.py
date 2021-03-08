66510dd3e63b8c05dbde34400228c0bb3afc59a4
HYEEWON <hw0916@naver.com>
2021-03-08 23:54:41 +0900

    Update binary Search

---
+++ programmers_kit/210308_징검다리_이분탐색_L4.py
@@ -0,0 +1,13 @@
+def solution(distance, rocks, n):
+    answer = 0
+
+    rocks.sort()
+
+    start, end = 0, distance
+    while start <= end:
+        mid = (start + end) // 2
+        
+
+
+
+    return answer
