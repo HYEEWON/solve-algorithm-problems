0c3695ee6b6436442f3ca6da0b8589a32ce6efdc
HYEEWON <hw0916@naver.com>
2021-03-12 00:19:57 +0900

    Update sort

---
+++ programmers_kit/210311_개쟁큰수_정렬_L2.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers = sorted(numbers, key=lambda x: x*3, reverse=True)
+    # 3번 정렬 -> 숫자가 1000 이하이기 때문
+    return str(int(''.join(numbers)))
+
+print(solution([3, 30, 34, 5, 9]))
+#print(solution([0, 0, 0, 0, 0]))
