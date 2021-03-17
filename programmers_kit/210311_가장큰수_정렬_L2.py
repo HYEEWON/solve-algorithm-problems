80b04bd4a7af5299db2c7d7ec882118f34714fcb
HYEEWON <hw0916@naver.com>
2021-03-17 23:13:05 +0900

    Update sort

--- programmers_kit/210311_가장큰수_정렬_L2.py
+++ programmers_kit/210311_가장큰수_정렬_L2.py
@@ -1,6 +1,7 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers = sorted(numbers, key=lambda x: x*3, reverse=True)
+    numbers = sorted(numbers, key=lambda x: (x[0], x[1], x[2]), reverse=True)
+    #numbers = sorted(numbers, key=lambda x: x*3, reverse=True)
     # 3번 정렬 -> 숫자가 1000 이하이기 때문
     return str(int(''.join(numbers)))
 
---
+++ programmers_kit/210312_H-Index_정렬_L2.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    answer = 0
+    citations = sorted(citations, reverse=True)
+
+    for i in range(len(citations)):
+        if citations[i] <= i:
+            answer = i
+            break
+    if i == len(citations):
+        return i;
+    return answer
+
+print(solution([3, 0, 6, 1, 5]))
+3w0317
