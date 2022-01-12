62f8ab35082d7022512d1cb387ac739f156fc693
HYEEWON <hw0916@naver.com>
2022-01-12 21:14:38 +0900

    Add weekly 11th

--- programmers/weekly_challange/220111_11주차_아이템줍기_L3.py
+++ programmers/weekly_challange/220111_11주차_아이템줍기_L3.py
@@ -9,12 +9,10 @@ from collections import deque
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
     # 좌표*2
-    board = [[0 for j in range(101)] for i in range(101)]
-    visit = [[False for j in range(101)] for i in range(101)]
-    answer = 0
+    board = [[0 for j in range(1001)] for i in range(1001)]
 
     # 사각형의 테두리+내부를 1로 채움
-    for x1, y1, x2, y2 in rectangle:
+    for x1, y1, x2, y2 in rectangle: # [좌측 하단 x, 좌측 하단 y, 우측 상단 x, 우측 상단 y]
         for i in range(2*y1, 2*y2+1):
             for j in range(2*x1, 2*x2+1):
                 board[i][j] = 1
@@ -29,25 +27,15 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
 
     # BFS
     q = deque()
-    q.append((0, characterX, characterY))
+    q.append((0, characterY, characterX))
 
     while q:
-        cnt, x, y = q.popleft()
-        visit[y][x] = True
+        cnt, y, x = q.popleft()
+        board[y][x] = -1
 
-        if x == itemX and y == itemY:
-            answer = cnt // 2
+        if board[itemY][itemX] < 0:
+            return cnt // 2 # 좌표를 2배했으므로 2로 나눔
 
         for dy, dx in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            if visit[y+dy][x+dx]:
-                continue
-
             if board[y+dy][x+dx] > 0:
-                q.append((cnt+1, x+dx, y+dy))
-
-    # 좌표를 2배했으므로 2로 나눔
-    return answer
-
-
-print(solution([[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]], 1, 3, 7, 8))
-
+                q.append((cnt+1, y+dy, x+dx))
