442d60a41fd63233e4f3a23c7789313e195ccc97
HYEEWON <hw0916@naver.com>
2022-02-26 23:56:48 +0900

    Add 121 array, dp

---
+++ leetcode/220226_121_BestTimetoBuyandSellStock_Easy.py
@@ -0,0 +1,27 @@
+# Array, DP
+
+class Solution:
+    def maxProfit(prices):
+        answer = 0
+        min_buy = prices[0]
+
+        for i in range(1, len(prices)):
+            min_buy = min(min_buy, prices[i])
+            answer = max(answer, prices[i] - min_buy)
+
+        return answer
+
+        '''
+        dp = [0 for i in range(len(prices))]
+        min_buy = prices[0]
+
+        for i in range(1, len(prices)):
+            if prices[i] - min_buy > 0:
+                dp[i] = max(dp[i - 1], prices[i] - min_buy)
+            if min_buy > prices[i]:
+                min_buy = prices[i]
+                max_sell = 0
+            dp[i] = max(dp[i - 1], dp[i])
+        return dp[len(prices) - 1]
+        '''
+        
