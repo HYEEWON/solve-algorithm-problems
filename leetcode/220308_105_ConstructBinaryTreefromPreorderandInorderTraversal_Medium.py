de349c64c7afa147f1a450fb87cbbb4ba3766489
HYEEWON <hw0916@naver.com>
2022-03-09 20:29:05 +0900

    Add #105 binary tree, divide and conquer

---
+++ leetcode/220308_105_ConstructBinaryTreefromPreorderandInorderTraversal_Medium.py
@@ -0,0 +1,18 @@
+# Tree, Binary Tree, Divide and Conquer, Array, Hash Table
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        idx = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1:idx+1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+        return root
+
