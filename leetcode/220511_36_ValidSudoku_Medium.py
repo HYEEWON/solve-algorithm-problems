bd1db3b5f11ee53ad74b2512e7f73d8d28470b9d
HYEEWON <hw0916@naver.com>
2022-05-11 23:59:53 +0900

    Add #36 array, hash table

--- leetcode/220511_36_ValidSudoku_Medium.py
+++ leetcode/220511_36_ValidSudoku_Medium.py
@@ -34,3 +34,21 @@ class Solution:
                 s_x += 3
             s_y += 3
         return True
+
+    # 참고
+    # https://leetcode.com/problems/valid-sudoku/discuss/15509/Clean-and-Easy82ms-Python
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        flag = set()
+        for y in range(9):
+            for x in range(9):
+                if board[y][x] != '.':
+                    # x줄에 board[y][x]가 있음, y 컬럼에 board[y][x]가 있음
+                    # 각 구역에 board[y][x]가 있음 ex) (0, 0), (0, 1) 등
+                    if (x, board[y][x]) in flag or (board[y][x], y) in flag \
+                            or (y//3, x//3, board[y][x]) in flag:
+                        return False
+                    flag.add((x, board[y][x]))
+                    flag.add((board[y][x], y))
+                    flag.add((y//3, x//3, board[y][x]))
+
+        return True
