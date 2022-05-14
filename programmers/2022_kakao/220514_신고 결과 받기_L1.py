34af2ac544a685cd9b9ac8480aeecf99f99e42ca
HYEEWON <hw0916@naver.com>
2022-05-14 12:04:38 +0900

    Add 2022kakao #1 hash table

---
+++ programmers/2022_kakao/220514_신고 결과 받기_L1.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+def solution(id_list, report, k):
+    answer = [0 for i in range(len(id_list))]
+
+    d = defaultdict(set)
+    index = defaultdict(int)
+
+    for i, ids in enumerate(id_list):
+        index[ids] = i
+
+    for r in set(report):
+        r = r.split()
+        d[r[1]].add(r[0])
+
+    for key, value in zip(d.keys(), d.values()):
+        if len(value) >= k:
+            for name in d[key]:
+                answer[index[name]] += 1
+
+    return answer
