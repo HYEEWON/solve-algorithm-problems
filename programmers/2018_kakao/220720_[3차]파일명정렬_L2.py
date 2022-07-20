5e2613adb83a31ed036b02370059a06c5e41755c
HYEEWON <hw0916@naver.com>
2022-07-20 16:44:07 +0900

    Add 2018kakao #11 string, regex

--- programmers/2018_kakao/220720_[3차]파일명정렬_L2.py
+++ programmers/2018_kakao/220720_[3차]파일명정렬_L2.py
@@ -16,3 +16,14 @@ def solution(files):
     for i, new_file in enumerate(new_files):
         answer.append(files[new_file[-1]])
     return answer
+
+
+def solution2(files):
+    p = re.compile('([\D]+)([\d]+)(.*)')
+    new_files = []
+    for i, file in enumerate(files):
+        tmp = list(p.findall(file)[0])
+        new_files.append(tmp)
+    new_files.sort(key=lambda x: (x[0].lower(), int(x[1])))
+
+    return [''.join(s) for s in new_files]
