3550b023808bea6aa4a969195cddfe80ab3296a5
HYEEWON <hw0916@naver.com>
2022-09-09 22:00:26 +0900

    Add 2022kakao dfs

--- programmers/2022_kakao/220819_양과늑대_L3.py
+++ programmers/2022_kakao/220819_양과늑대_L3.py
@@ -1,3 +1,6 @@
+# DFS
+# 방문할 수 있는 노드들을 계속 가지고 있어야 함
+
 from collections import defaultdict
 from copy import deepcopy
 
@@ -7,7 +10,6 @@ def dfs(info, tree, node, cnt, visit, available):
     if cnt[0] <= cnt[1]:
         return
     answer = max(answer, cnt[0])
-    print(node, cnt, available, "  #########")
     for t in tree[node]:
         if not visit[t]:
             available.append(t)
@@ -20,8 +22,6 @@ def dfs(info, tree, node, cnt, visit, available):
         dfs(info, tree, t, cnt, visit, tmp)
         visit[t] = False
         cnt[info[t]] -= 1
-        print(node, cnt, available)
-
 
 def solution(info, edges):
     global answer
@@ -30,11 +30,9 @@ def solution(info, edges):
     for edge in edges:
         tree[edge[0]].append(edge[1])
         tree[edge[1]].append(edge[0])
-    print(tree)
+
     visit = [False for i in range(len(tree.keys()))]
     visit[0] = True
     dfs(info, tree, 0, [1, 0], visit, [])
 
     return answer
-
-print(solution([0,0,1,1,1,0,1,0,1,0,1,1], [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]))
